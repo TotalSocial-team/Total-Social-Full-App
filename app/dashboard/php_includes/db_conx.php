aa3b0d573ad9cea8128983506651b54fe21fd197
TotalSocial-team <haulexeyo15@gmail.com>
2017-03-13 21:46:05 -0400

    Update db_conx.php

--- app/dashboard/php_includes/db_conx.php
+++ app/dashboard/php_includes/db_conx.php
@@ -1,5 +1,5 @@
 <?php
-$db_conx = mysqli_connect("localhost", "tsnetwo2_ts", "Diandian123@", "tsnetwo2_totalsocial");
+$db_conx = mysqli_connect("127.11.125.2:3306", "adminiR7LmEy", "1GlEM4bBnUKu", "totalsocial");
 // Evaluate the connection
 if (mysqli_connect_errno()) {
     echo mysqli_connect_error();
